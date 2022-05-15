7c3317282cab527b29f3a52e8b4bf55c06f56040
shishi <eksmok@hotmail.fr>
2022-05-15 22:44:14 +0200

    Begin of gini_computation

--- src/datapoint.py
+++ src/datapoint.py
@@ -1,6 +1,15 @@
-from typing import Dict
+from typing import Dict, List
 
 
 class DataPoint:
     def __init__(self, data: Dict[str, float]):
         self.data = data
+
+    @staticmethod
+    def computation_gini(data: List['DataPoint'], feature: str) -> float:
+        pass
+
+    @staticmethod
+    def _get_feature(feature: str, data: List['DataPoint']):
+        list_feature: List[float] = []
+        pass
