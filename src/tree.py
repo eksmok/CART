1b7f7f9d503bbe13316c65a9ac15595bcb86cf85
shishi <eksmok@hotmail.fr>
2022-05-21 15:44:27 +0200

    Compute of the gini index done

--- src/tree.py
+++ src/tree.py
@@ -1,5 +1,22 @@
-from typing import List
+# from typing import List
+from src.datapoint import DataPoint
+
+
 class Node:
-    def __init__(self, data : List['DataPoint'], Edge):
-        self.data = data
+    def __init__(self, datapoint: DataPoint):
+        """
+
+        :param datapoint:
+        """
+        self.datapoint = datapoint
+
+
+    def information_gain(self, feature: str, datapoint: DataPoint) -> float:
+        """
+
+        :param feature:
+        :param datapoint:
+        :return:
+        """
+
 
